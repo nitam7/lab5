e8826794b894893db948f4ae9852d4fa2ee2e79d
nitam7 <nitam@ucsd.edu>
2018-02-08 10:52:54 -0800

    got to slide 22

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -14,33 +14,35 @@ function initializePage() {
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
-	
-	if (name == "Doug Engelbart") {
-		return "Notable Grudge";
-	} 
-	else if (name == "Ivan Sutherland") {
-		return "Vandal Heist Run";
-	}
-	else if (name == "JCR Licklider") {
-		return "Crick Rid Jell";
-	}
-	else if (name == "Vannevar Bush") {
-		return "Ravens Van Hub";
-	}
-	else if (name == "Alan C. Kay") {
-		return "Canal Yak";
-	}
-	else if (name == "Allen Newell") {
-		return "Ellen All New";
-	}
-	else if (name == "Lucy Suchman") {
-		return "Lunacy Chums";
-	}
-	else if (name == "Grace Hopper") {
-		return "Gear Chopper";
-	}
-	else {
-		console.log(name + " not known for anagramming.");
-		return name;
-	}
+	name.addEventListener("click", function() {
+
+		if (name == "Doug Engelbart") {
+			return "Notable Grudge";
+		} 
+		else if (name == "Ivan Sutherland") {
+			return "Vandal Heist Run";
+		}
+		else if (name == "JCR Licklider") {
+			return "Crick Rid Jell";
+		}
+		else if (name == "Vannevar Bush") {
+			return "Ravens Van Hub";
+		}
+		else if (name == "Alan C. Kay") {
+			return "Canal Yak";
+		}
+		else if (name == "Allen Newell") {
+			return "Ellen All New";
+		}
+		else if (name == "Lucy Suchman") {
+			return "Lunacy Chums";
+		}
+		else if (name == "Grace Hopper") {
+			return "Gear Chopper";
+		}
+		else {
+			console.log(name + " not known for anagramming.");
+			return name;
+		}
+	});
 }
